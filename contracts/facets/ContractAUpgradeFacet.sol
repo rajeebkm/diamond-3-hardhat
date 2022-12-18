55a2d2dfab3ee45842c23d552228afc5d6b304ba
Rajeeb Kumar Malik <97892873+rajeebkm@users.noreply.github.com>
2022-12-19 01:45:31 +0530

    Update ContractAUpgradeFacet

--- contracts/facets/ContractAUpgradeFacet.sol
+++ contracts/facets/ContractAUpgradeFacet.sol
@@ -1,25 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 
-import "./ContractAFacet.sol";
-import "./ContractBFacet.sol";
-// import "../libraries/ContractAAppStorage.sol";
 
-contract ContractAUpgradeFacet is ContractAFacet, ContractBFacet {
-    // ContractAAppStorage internal s;
+import { ContractAFacet } from "./ContractAFacet.sol";
+// import { AccessRegistry } from "../AccessRegistry.sol";
+import { LibDiamond } from "../libraries/LibDiamond.sol";
+import { IAccessRegistry } from "../interfaces/IAccessRegistry.sol";
 
-    bytes32 public MANAGER_ROLE = keccak256("MANAGER");
+contract ContractAUpgradeFacet is ContractAFacet {
 
-    constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(MANAGER_ROLE, msg.sender);
+    // function initialize() public virtual override initializer {
+    //     AppStorage storage ds = LibCommon.diamondStorage();
+    //     ds.MANAGER_ROLE = keccak256("MANAGER");
+    //     addAdminRole(ds.MANAGER_ROLE, _msgSender());
+    //     addRole(ds.MANAGER_ROLE, _msgSender());
+    //     // addAdminRole(MANAGER_ROLE, address(this));
+        
+    // }
 
-    }
+
     
-    function set(uint256 _value) public virtual override onlyOwner nonReentrant {
-        require(hasRole(MANAGER_ROLE, msg.sender), 'Only Manager can call this');
-        s.value += _value;
+    
+    function set(uint256 _newValue) public virtual override nonReentrant {
+        
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+         require(
+            IAccessRegistry(ds.superAdminAddress).hasAdminRole(ds.superAdmin, _msgSender()) ||
+                IAccessRegistry(ds.superAdminAddress).hasAdminRole(ds.MANAGER_ROLE, _msgSender()),
+            "ERROR: Not an admin"
+        );
+        // require(hasAdminRole(ds.MANAGER_ROLE, _msgSender()), 'Only Manager can call this');
+        // require(hasAdminRole(ds.superAdmin, _msgSender()), 'Only Manager can call this');
+        LibDiamond._updatevalue(_newValue);
+        emit LibDiamond.ValueUpdated(_newValue);
     }
 
 
